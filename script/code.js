10d92176c73e0d5afadd976ea02d0574be8a592d
Erin-Caitlin <erinwilliamscat@gmail.com>
2024-05-30 11:21:02 +0200

    function

---
+++ script/code.js
@@ -0,0 +1,24 @@
+let first_number = 15
+let second_number = 17
+console.log(`First Value: ${first_number}`);
+console.log(`Second Value: ${second_number}`);
+
+// First Swap
+firstValue = (first_number + 2)
+secondValue = (second_number - 2)
+console.log(`first_number1 = ${firstValue}`);
+console.log(`second_number1 = ${secondValue}`);
+
+// Second Swap
+let keira = first_number + second_number
+second_number = keira - second_number
+first_number = keira - first_number
+console.log(`first_number2 = ${first_number}`);
+console.log(`second_number2 = ${second_number}`);
+
+// Third Swap
+let firstSwap = first_number
+first_number = second_number
+second_number = first_number
+console.log(`first_number3 = ${firstSwap}`);
+console.log(`second_number3 = ${second_number}`);
